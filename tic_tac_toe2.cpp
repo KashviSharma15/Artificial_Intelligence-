6d5c05793279c200d146353b39f50a042a732fb9
Kashvi Sharma <kashvi.sharma5944@gmail.com>
2025-08-12 20:18:30 +0530

    Add files via upload

---
+++ tic_tac_toe2.cpp
@@ -0,0 +1,128 @@
+#include <iostream>
+using namespace std;
+
+int BOARD[10]; 
+int WIN_LINES[8][3] = {
+    {1, 2, 3}, 
+    {4, 5, 6},
+    {7, 8, 9},
+    {1, 4, 7}, 
+    {2, 5, 8},
+    {3, 6, 9},
+    {1, 5, 9}, 
+    {3, 5, 7}
+};
+
+void displayBoard() {
+    for (int i = 1; i <= 9; i++) {
+        char symbol;
+        if (BOARD[i] == 2) symbol = '_';
+        else if (BOARD[i] == 3) symbol = 'X';
+        else symbol = 'O';
+        cout << symbol << " ";
+        if (i % 3 == 0) cout << endl;
+    }
+    cout << endl;
+}
+
+int MAKE2() {
+    if (BOARD[5] == 2) return 5; 
+    int sides[] = {2, 4, 6, 8};
+    for (int pos : sides) {
+        if (BOARD[pos] == 2) return pos;
+    }
+    return 0;
+}
+
+int POSSWIN(int player) {
+    int targetProduct = (player == 3) ? 18 : 50; 
+    for (int i = 0; i < 8; i++) {
+        int p1 = WIN_LINES[i][0], p2 = WIN_LINES[i][1], p3 = WIN_LINES[i][2];
+        int prod = BOARD[p1] * BOARD[p2] * BOARD[p3];
+        if (prod == targetProduct) {
+            if (BOARD[p1] == 2) return p1;
+            if (BOARD[p2] == 2) return p2;
+            if (BOARD[p3] == 2) return p3;
+        }
+    }
+    return 0;
+}
+
+void GO(int pos, int player) {
+    if (pos >=1 && pos <=9 && BOARD[pos] == 2) BOARD[pos] = player;
+}
+
+bool checkWin(int player) {
+    int productTarget = (player == 3) ? 27 : 125; 
+    for (int i = 0; i < 8; i++) {
+        int p1 = WIN_LINES[i][0], p2 = WIN_LINES[i][1], p3 = WIN_LINES[i][2];
+        int prod = BOARD[p1] * BOARD[p2] * BOARD[p3];
+        if (prod == productTarget) return true;
+    }
+    return false;
+}
+
+bool isDraw() {
+    for (int i = 1; i <= 9; i++) {
+        if (BOARD[i] == 2) return false;
+    }
+    return true;
+}
+
+int main() {
+    for (int i = 1; i <= 9; i++) BOARD[i] = 2;
+
+    cout << "You are O. Computer is X." << endl;
+    displayBoard();
+
+    while (true) {
+        int humanMove;
+        cout << "Enter your move (1-9): ";
+        while (true) {
+            cin >> humanMove;
+            if (humanMove >= 1 && humanMove <= 9 && BOARD[humanMove] == 2) {
+                GO(humanMove, 5); 
+                break;
+            } else {
+                cout << "Invalid move, try again: ";
+            }
+        }
+        displayBoard();
+
+        if (checkWin(5)) {
+            cout << "You won!" << endl;
+            break;
+        }
+        if (isDraw()) {
+            cout << "Game is a draw!" << endl;
+            break;
+        }
+
+        int aiMove = POSSWIN(3); 
+        if (!aiMove) aiMove = POSSWIN(5); 
+        if (!aiMove) aiMove = MAKE2(); 
+        if (!aiMove) {
+            for (int i = 1; i <= 9; i++) {
+                if (BOARD[i] == 2) {
+                    aiMove = i;
+                    break;
+                }
+            }
+        }
+        cout << "AI moves at position: " << aiMove << endl;
+        GO(aiMove, 3);
+        displayBoard();
+
+        if (checkWin(3)) {
+            cout << "Computer won! " << endl;
+            break;
+        }
+        if (isDraw()) {
+            cout << "Game is a draw!" << endl;
+            break;
+        }
+    }
+
+    cout << "Game Over!" << endl;
+    return 0;
+}
